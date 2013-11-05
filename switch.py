d7aa1b347e902d4b3137c255569892450043bef1
vinhyl <vincent.hui.xyl@gmail.com>
2013-11-05 10:10:59 +0800

    first commit

---
+++ switch.py
@@ -0,0 +1,26 @@
+#coding: utf8
+import sys
+import RPi.GPIO as GPIO
+import time
+
+PORT = 7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PORT, GPIO.OUT)
+
+
+def high():
+    GPIO.output(PORT, GPIO.HIGH)
+
+
+def low():
+    GPIO.output(PORT, GPIO.LOW)
+
+
+if sys.argv[1] == "open":
+    low()
+    time.sleep(2)
+    high()
+    GPIO.cleanup()
+elif sys.argv[1] == "close":
+    GPIO.cleanup()
